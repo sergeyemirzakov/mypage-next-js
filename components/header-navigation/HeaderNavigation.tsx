aaf59cfcc69497023f4bc5e2f2849a7c8d308781
sergeyemirzakov <im.seem16@gmail.com>
2022-04-08 09:58:37 +0500

    feat: Create HeaderNavigation Component

---
+++ components/header-navigation/HeaderNavigation.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+
+import { HeaderNavigation } from './HeaderNavigation.props';
+
+const menuItems: string[] = ['Our story', 'Membership', 'Write'];
+
+const HeaderNavigation = ({ scrollHeight }: HeaderNavigation) => {
+  const buttonStyles = scrollHeight ? 'bg-black' : 'bg-green-600';
+
+  return (
+    <>
+      <nav className="flex items-center text-sm">
+        <ul className="flex">
+          {menuItems &&
+            menuItems.map((item, idx) => (
+              <li key={idx} className="mr-7 hidden sm:block">
+                <Link href="/">
+                  <a>{item}</a>
+                </Link>
+              </li>
+            ))}
+          <li className="mr-7">
+            <Link href="/">
+              <a>Sign in</a>
+            </Link>
+          </li>
+        </ul>
+        <button
+          className={
+            buttonStyles +
+            ' text-white py-2 px-5 rounded-2xl transition duration-300'
+          }
+        >
+          Get started
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default HeaderNavigation;
