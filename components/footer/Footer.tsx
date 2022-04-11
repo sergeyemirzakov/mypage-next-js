625967895cd22193aa7e5cc0468448da159158da
sergeyemirzakov <im.seem16@gmail.com>
2022-04-11 14:32:03 +0500

    add: Footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 p-1 text-center mt-10">
+      <div className="container">
+        <span className="text-white">
+          I am footer and will be always at the bottom 😉
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
