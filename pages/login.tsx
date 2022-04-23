535a0e200d98f74fe3f61a322d8462a22a3abbf4
sergeyemirzakov <im.seem16@gmail.com>
2022-04-23 11:02:49 +0500

    feat: Create Login and Register pages

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '../components/forms/LoginForm';
+
+const Login = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
---
+++ pages/register.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from '../components/forms/RegisterForm';
+
+const register = () => {
+  return (
+    <>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default register;
