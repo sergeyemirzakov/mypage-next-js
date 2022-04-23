02ba8b3d8f870f9068162cbbf9bada7c51bb40db
sergeyemirzakov <im.seem16@gmail.com>
2022-04-23 11:08:21 +0500

    chore: Endpoints

---
+++ lib/endpoints/endpoints.ts
@@ -0,0 +1,8 @@
+// User
+export const REGISTER_USER = 'https://api.realworld.io/api/users';
+export const LOGIN_USER = 'https://api.realworld.io/api/users/login';
+export const GET_CURRENT_USER = 'https://api.realworld.io/api/user';
+// Articles
+export const ARTICLES_API = `https://api.realworld.io/api/articles`;
+// Tags
+export const TAGS_API = `https://api.realworld.io/api/tags`;
