c5f1659d098b1da3c31bc7805d23dd85288051c1
sergeyemirzakov <im.seem16@gmail.com>
2022-04-23 10:59:30 +0500

    refactor: Refactor MainScreen component

--- components/main-screen/MainScreen.tsx
+++ components/main-screen/MainScreen.tsx
@@ -1,8 +1,14 @@
-import { MainScreenProps } from './MainScreen.props';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { storage } from '../../lib/utils/storage';
 
-const MainScreen = ({ openDialogModal }: MainScreenProps) => {
+const MainScreen = () => {
+  const router = useRouter();
+  const { data: user } = useSWR('user', storage);
+  const isLoggedIn = !!user;
+
   return (
-    <div className={'bg-blue-200 pt-20 border-b-1 border-black'}>
+    <div className={'bg-blue-300 pt-15 border-b-1 border-black'}>
       <div className="container">
         <div className="py-28 bg-none md:bg-[url('/images/bg.png')] bg-[length:420px] bg-right-bottom bg-no-repeat">
           <h1 className={'font-serif text-6xl mb-10 sm:text-7xl md:text-8xl'}>
@@ -11,12 +17,15 @@ const MainScreen = ({ openDialogModal }: MainScreenProps) => {
           <h2 className="text-xl sm:text-2xl max-w-md mb-10">
             Discover stories, thinking, and expertise from writers on any topic.
           </h2>
-          <button
-            onClick={openDialogModal}
-            className="bg-black text-white py-3 px-10 rounded-2xl"
-          >
-            Start reading
-          </button>
+          {/* If the user is logged in we have to hide this button otherwise show it */}
+          {!isLoggedIn && (
+            <button
+              onClick={() => router.push('/login')}
+              className="bg-black text-white py-3 px-10 rounded-2xl"
+            >
+              Start reading
+            </button>
+          )}
         </div>
       </div>
     </div>
