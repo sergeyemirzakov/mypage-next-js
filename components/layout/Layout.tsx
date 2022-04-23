c862dacdbc709d769e61f34efa55c9b8707f870c
sergeyemirzakov <im.seem16@gmail.com>
2022-04-23 10:55:42 +0500

    refactor: Refactor Layout component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,45 +1,19 @@
-import { useState } from 'react';
-
 import Header from '../header/Header';
-import MainScreen from '../main-screen/MainScreen';
-import Article from '../article/Article';
-import Sidebar from '../sidebar/Sidebar';
-import DialogModal from '../dialog-modal/DialogModal';
 import Footer from '../footer/Footer';
 import { LayoutProps } from './Layout.props';
 
 const Layout = ({ children }: LayoutProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openDialogModal = () => {
-    setIsOpen(true);
-  };
-
-  const closeDialogModal = () => {
-    setIsOpen(false);
-  };
-
   return (
     <>
-      <Header openDialogModal={openDialogModal} />
-      <MainScreen openDialogModal={openDialogModal} />
-      {isOpen && (
-        <DialogModal isOpen={isOpen} closeDialogModal={closeDialogModal} />
-      )}
-      <div className="pt-20">
-        <div className="container">
-          <div className="flex flex-col md:flex md:flex-row">
-            <Article />
-            <Sidebar />
-          </div>
-        </div>
-      </div>
-
+      {/* Header */}
+      <Header />
       <div className="flex flex-col justify-between">
+        {/* Main */}
         <main className="mt-20">
-          <div className="container">{children}</div>
+          <div>{children}</div>
         </main>
-        <Footer />
+        {/* Footer */}
+        {/* <Footer /> */}
       </div>
     </>
   );
