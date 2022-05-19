3d33bfe44b6dd31ceb0d5f5313cdc9fb265d9ddf
sergeyemirzakov <im.seem16@gmail.com>
2022-05-19 13:46:45 +0500

    feat: Create ListArticles

---
+++ components/articles/ArticlesBody.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+interface ArticleBodyProps {
+  title: string;
+  description: string;
+  slug: string;
+}
+
+const ArticleBody = ({ title, description, slug }: ArticleBodyProps) => {
+  return (
+    <div className="block">
+      <Link href={`/articles/${slug}`}>
+        <a>
+          <h3 className="sm:text-md md:text-xl font-bold">{title}</h3>
+          <p className="hidden sm:block mt-1 text-sm text-gray-500">
+            {description.length > 200
+              ? description?.slice(0, 200) + '...'
+              : description}
+          </p>
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default ArticleBody;
