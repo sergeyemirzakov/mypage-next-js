be3cf5338e6e0b8734f2e8bf9c5bd782c0359130
sergeyemirzakov <im.seem16@gmail.com>
2022-05-19 13:53:04 +0500

    refactor: Refactor of component

--- components/tags/TagsList.tsx
+++ components/tags/TagsList.tsx
@@ -10,7 +10,7 @@ const TagsList = ({ tags }: TagsListProps) => {
       {!!tags &&
         tags.map((item, idx) => (
           <Link key={idx} href="/">
-            <a className="text-slate-500 border border-slate-200 py-2 px-4 mr-2 mb-2 text-xs">
+            <a className="text-slate-500 border border-slate-200 py-2 px-4 mr-2 mb-2 text-xs rounded-md">
               {item}
             </a>
           </Link>
