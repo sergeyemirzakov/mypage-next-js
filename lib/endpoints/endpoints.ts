d7cc9573cb0c11dc0adc1b8317412db74007e183
sergeyemirzakov <im.seem16@gmail.com>
2022-05-19 13:51:47 +0500

    refactor: Refactor of endpoints

--- lib/endpoints/endpoints.ts
+++ lib/endpoints/endpoints.ts
@@ -1,8 +1,8 @@
 // User
-export const REGISTER_USER = 'https://api.realworld.io/api/users';
-export const LOGIN_USER = 'https://api.realworld.io/api/users/login';
-export const GET_CURRENT_USER = 'https://api.realworld.io/api/user';
+export const REGISTER_USER = process.env.NEXT_PUBLIC_REGISTER_USER || '';
+export const LOGIN_USER = process.env.NEXT_PUBLIC_LOGIN_USER || '';
+export const GET_CURRENT_USER = process.env.NEXT_PUBLIC_GET_CURRENT_USER || '';
 // Articles
-export const ARTICLES_API = `https://api.realworld.io/api/articles`;
+export const ARTICLES_API = process.env.NEXT_PUBLIC_ARTICLE_API || '';
 // Tags
-export const TAGS_API = `https://api.realworld.io/api/tags`;
+export const TAGS_API = process.env.NEXT_PUBLIC_TAGS_API || '';
