556d362281b5cd68da07884190ecfb207200cbe4
sergeyemirzakov <im.seem16@gmail.com>
2022-05-19 13:55:15 +0500

    refactor: Refactor of component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import type { AppProps } from 'next/app';
-// import { Provider } from 'react-redux';
-// import { store } from '../store/store';
 import '../styles/globals.css';
 import Router from 'next/router';
 import Preloader from '../components/preloaders/Preloader';
