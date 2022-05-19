3e3a98815d8af9931f33501cbaa4c8c82ab01903
sergeyemirzakov <im.seem16@gmail.com>
2022-05-19 13:19:36 +0500

    refactor: Refactor Tags component

--- components/tags/Tags.tsx
+++ components/tags/Tags.tsx
@@ -1,14 +1,15 @@
 import useSWR from 'swr';
-import { tagsFetcher } from '../../lib/api/tags';
 import TagsList from './TagsList';
 import Spinner from '../preloaders/Spinner';
+import fetcher from '../../lib/utils/fetcher';
 
 const Tags = () => {
-  const { data, error } = useSWR('tags', tagsFetcher);
+  const { data, error } = useSWR(process.env.NEXT_PUBLIC_TAGS_API, fetcher);
+
   return (
     <div className="flex md:flex flex-wrap pb-7">
       {error && (
-        <h1 className="text-md font-semibold text-red-500">Server error</h1>
+        <h2 className="text-md font-semibold text-red-500">Server error</h2>
       )}
       {!data && !error && <Spinner color="black" size="30" />}
       {!!data && <TagsList tags={data.tags} />}
